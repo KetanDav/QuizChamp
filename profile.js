3e96a73563ed2510fe2a2abbdbe2daef8680b843
KetanDav <davketan029@gmail.com>
2024-04-16 10:06:46 +0530

    Small Changes

--- profile.js
+++ profile.js
@@ -1,5 +1,13 @@
 sQuiz = document.querySelector(".quiz");
+sHistory = document.querySelector(".history");
+sBookmark = document.querySelector(".bookmark");
 
 sQuiz.addEventListener("click",()=>{
     window.location.href = "Quiz.html";
+})
+sHistory.addEventListener("click",()=>{
+    window.location.href = "History.html";
+})
+sBookmark.addEventListener("click",()=>{
+    window.location.href = "Bookmark.html";
 })
